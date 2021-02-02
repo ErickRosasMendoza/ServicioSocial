6f5be3e5f323a43de364f458e7985caec9100191
DESKTOP-NDS8TEB\luis <megatromps@gmail.com>
2021-02-01 21:00:31 -0600

    no message

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,59 +1,52 @@
 import React from 'react';
 import Slider from './Slider';
-import SideBar from './SideBar';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 class Formulario extends React.Component {
-
-    mailRef = React.createRef();
-    contraseñaRef = React.createRef();
-
-
+    nombreRef = React.createRef();
+    idRef = React.createRef();
     state = {
         user:{}
     };
 
 
-    recibirFormulario = (evento) => {
 
-        evento.preventDefault();
-       
-      
 
-        
-        var user = {
-            mail:this.mailRef.current.value,
-            contraseña:this.contraseñaRef.current.value,
-        };
-        console.log(user);
-        this.setState({
-            user: user
-        });
+        changeState = () => {
+            this.setState({
+                user: {
+                    id: this.idRef.current.value,
+                    nombre: this.nombreRef.current.value,
     
-
+                }
+    
+            });
+            console.log(this.state);
         }
+
+
+        enviarUsuario = (e) => {
+            this.changeState();
+            axios.post("http://localhost:8080/cliente/logging", this.state.user)
+                .then(res => {
+                    console.log(this.state);
+                });
+            }
 
 
 
 
     render() {
-        console.log(this.props)
-        if(this.state.user.contraseña){
-            var user = this.state.user;
-        }
+
 
         return (
    
   
-            <form id="formulario" onSubmit={this.recibirFormulario} onChange = {this.recibirFormulario}  >
-                 { this.state.user.nombre &&
-                <div id = "user-data">
-                <p><strong>e-mail: {user.mail}</strong></p>
-                <p><strong>contraseña: {user.contraseña}</strong></p>
-                <p><strong>Descripcion: {user.descripcion}</strong></p>
-                <p><strong>Genero: {user.genero}</strong></p>
-                </div>
-
-            }
+            <form id="formulario"  >
+              
+              
+            
 
                 <Slider
                     title="iniciar sessión"
@@ -67,20 +60,20 @@ class Formulario extends React.Component {
 
 
                     <div className="form-group" >
-                        <label htmlFor="mail" >E-mail</label><br />
-                        <input type="text" name="mail" ref={this.mailRef} />
+                        <label htmlFor="nombre" >E-mail</label><br />
+                        <input type="text" name="mail" ref={this.nombreRef} onChange={this.changeState} />
                     </div>
 
                     <div className="form-group">
-                        <label htmlFor="contraseña" >Contraseña</label><br />
-                        <input type="text" name="contraseña"  ref={this.contraseñaRef} />
+                        <label htmlFor="id" >Contraseña</label><br />
+                        <input type="text" name="contraseña" ref={this.idRef} onChange={this.changeState}/>
                     </div>
 
 
 
 
                     <br />
-                    <input type="submit" value="Aceptar" className="btn btn-success form-radio" />
+                    <input type="submit" value="Aceptar" className="btn btn-success form-radio" onClick = {this.enviarUsuario} />
 
 
 
