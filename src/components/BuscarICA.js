6c74bac5305873366bbbc0857bf984154bbc283a
DESKTOP-VBS8BFQ\Erick <erosasm1200@alumno.ipn.mx>
2021-02-16 19:17:09 -0600

    Cambios al mismo tiempo

--- src/components/BuscarICA.js
+++ src/components/BuscarICA.js
@@ -50,8 +50,9 @@ class BuscarICA extends React.Component {
                                             <td className="table_lista">{programa1.nombre}</td>
                                             <td className="table_lista">{programa1.apellidoPaterno}</td>
                                             <td className="table_lista">{programa1.apellidoMaterno}</td>
-                                            <td className="table_lista"><Link to={'/AlumnoDetalle/' + programa1.idAlumno}>{programa1.boleta}</Link></td>
+                                            <td className="table_lista">{programa1.boleta}</td>
                                             <td className="table_lista">{programa1.programaAcademico}</td>
+                                            <td><Link to={'/DirectorioArchivosAlumno/' + programa1.idAlumno} id="btn_watch">Ver Archivos</Link></td>
                                         </tr>
                                     </tbody>
                                 )}
