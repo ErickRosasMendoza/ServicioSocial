8dc6af171458cae01c65f0e642bfab387fb0a466
DESKTOP-NDS8TEB\luis <megatromps@gmail.com>
2021-02-17 22:21:18 -0600

    iniciar session sin bugs

--- src/components/IniciarSesion.js
+++ src/components/IniciarSesion.js
@@ -15,11 +15,14 @@ class IniciarSesion extends React.Component {
 
     url = Global.url;
 
+    emailRef = React.createRef();
+
     state = {
         email: cookies.get('email'),
         idUsuario: cookies.get('idUsuario'),
         statusPassword: "true",
         statusEmail:"true",
+        usuario:{},
         form: {
             idUsuario: "",
             email: "",
@@ -27,72 +30,123 @@ class IniciarSesion extends React.Component {
             tipoUsuario: ""
         }
     }
+    componentWillMount= () => {
+       this.setState(
+           {
+            email:cookies.get('email'),
+         
+           }
+       )
+    }
 
     handleChange = async (e) => {
         await this.setState({
-            form: {
-                ...this.state.form,
-                [e.target.name]: e.target.value
+            form: { 
+
+                     ...this.state.form,
+                   [e.target.name]: e.target.value
+                
+                  
             }
         })
+        cookies.set('email', this.state.form.email, { path: "/" });
+        
+       console.log(cookies.get('email') + "cookies");
+        var psw = cookies.set('contraseña', this.state.form.contraseña, {path:"/"});
+        psw = md5(cookies.get('contraseña'));
+        console.log(psw + "cookies contraseña con md5");
+        cookies.set('contraseña', psw, {phat: "/"})
+
     }
+
 
     passwordChange = () => {
         this.setState({
             form: {
                 contraseña: md5(this.state.form.contraseña)
+                
             }
+           
         })
-        //console.log(this.state.form.contraseña)
+        
+       
     }
 
     login = async (e) => {
         this.passwordChange();
-        await axios.get(this.url + "usuario/findEmail/" + this.state.form.email)
-            .then(response => {
-                return response.data;
-            })
-            .then(response => {
-                if (response.tipoUsuario == false) {
-                    if (this.state.form.contraseña == response.contraseña) {
-                        cookies.set('idUsuario', response.idUsuario, { path: "/" })
-                        cookies.set('email', response.email, { path: "/" })
-                        window.location.href = "./MisDatosAlumno";
-                    } else {
-                        this.setState({
-                            statusPassword: "false"
-                        });
-                        //  alert("VERIFIQUE SU CONTRASEÑA");
-                        console.log(this.state.statusPassword);
+        if(this.state.statusEmail =="true" || this.state.statusEmail == "false"){
+                  await axios.get(this.url + "usuario/findEmail/" + (this.state.form.email ||this.state.usuario.email))
+                        .then(response => {
+                            this.setState({
+                                               usuario: response.data
+                                         })
+                                         console.log(response.data.email + "response datasss")
+                                         console.log(this.state.form.email + "datos dentro del form")
+                                         console.log(this.state.usuario.email + "datos dentro de usuario")
+                                         
+                                        return response.data;
+                                        
+                                         })
 
-                    }
-                } else if (response.tipoUsuario == true) {
-                    if (this.state.form.contraseña == response.contraseña) {
-                        cookies.set('idUsuario', response.idUsuario, { path: "/" })
-                        cookies.set('email', response.email, { path: "/" })
-                        window.location.href = "./MisDatosAdmin";
-                    } else {
-                        this.setState({
-                            statusPassword:"false"
-                        })
-                        //alert("VERIFIQUE SU CONTRASEÑA");
-                      //  window.location.href = "./IniciarSesion";
-                    }
-                } else {
-                    window.location.href = "./IniciarSesion";
-                }
-            })
-            .catch(error => {
+                           .then(response => {
+                           if (this.state.usuario.tipoUsuario == false) {
+                            console.log(cookies.get('contraseña') + " contraseña de las cookies")
+                            console.log( this.state.usuario.contraseña+ "contraseña de usuario")
+                                  if (cookies.get('contraseña') == this.state.usuario.contraseña) {
+                                  //  alert("dentro del segundo if del segundo then")
+                                   console.log(cookies.get('contraseña') + " email de las cookies")
+                                   console.log( this.state.usuario.contraseña+ "contraseña de usuario")
+                                  // alert("dentro del segundo if del segundo then")
+                                         cookies.set('idUsuario', response.idUsuario, { path: "/" })
+                                        cookies.set('email', response.email, { path: "/" })
+                                         window.location.href = "./MisDatosAlumno";
+                                          
+                                       }
+                                          else {
+                                                this.setState({
+                                                 statusPassword: "false"
+                                                 });
+                                                // alert("VERIFIQUE SU CONTRASEÑA");
+                                                // console.log(this.state.statusPassword);
+                                                }
+                            
+                                               console.log(response.data + "response data del segundo then")
+                             
+                            
+
+
+                                    }
+                                    else if (this.state.usuario.tipoUsuario == true) {
+                                             if (cookies.get('contraseña') == this.state.usuario.contraseña){
+                                                 cookies.set('idUsuario', this.state.usuario.idUsuario, { path: "/" })
+                                                  cookies.set('email', this.state.usuario.email, { path: "/" })
+                                                 window.location.href = "./MisDatosAdmin";
+                                                   
+                                                  } 
+                                                   else {
+                                                        this.setState({
+                                                       statusPassword:"false"
+                                                          })
+                                                // alert("VERIFIQUE SU CONTRASEÑA");
+                                                 window.location.href = "./IniciarSesion"; 
+                                                    } 
+                                                }       
+                                          })  
+                           .catch(error => {
                
-                this.setState({
-                        statusEmail:"false",
-                       
-                })
-                //alert("VERIFIQUE SU CORREO");
-                //window.location.href = "./IniciarSesion";
-                console.log(error + "status mail" + this.state.email +  " error en consulta");
-            })
-    }
+                                             this.setState({
+                                                        statusEmail:"false",
+                                                            });
+                                     //alert("VERIFIQUE SU CORREO");
+                                       //window.location.href = "./IniciarSesion";
+                             console.log(error + "status mail" + this.state.email +  " error en consulta");
+                             
+                            })  
+                             
+                        }             
+        
+   }
+
 
     render() {
 
@@ -108,6 +162,7 @@ class IniciarSesion extends React.Component {
                 />
                  <Link to={'./Registrarse'} class="registrarse">Registrarse</Link>
                      <div id="sidebar">
+                     <div>
                              <img src={logo2} id="logo2" alt="politecnico" />
                              <br /> <br />
                              <a className="text-logo"><strong>Departamento de Extensión</strong>  </a>
@@ -117,38 +172,41 @@ class IniciarSesion extends React.Component {
                      <div className="input-border" >
                                  <br /> <br /> <br />
                      <label htmlFor="email" className="text_login">Correo electrónico </label>
-                     <input type="email" className="input_login" name="email" onChange={this.handleChange} placeholder="Ingresa aquí tu correo electrónico" />
+                     <input type="email" className="input_login" name="email"  onChange={this.handleChange} placeholder="Ingresa aquí tu correo electrónico" />
                      </div>
-                     <div>
-                         <label htmlFor="contraseña" className="text_login">Contraseña</label>
-                            <input type="password" className="input_login" name="contraseña" autoComplete="on" placeholder="Ingresa aquí tu contraseña" onChange={this.handleChange} />
-                     </div>
-                            <br />
-                                <button class="btn" onClick={this.login} >Aceptar</button>
-            
-           
-                                
                      {(() => {
-                         switch (this.state.statusPassword) {
-                        case "false":
-                            return (
-                                <a className="warning">contraseña erronea</a>
-                            );
-                         }
                          switch(this.state.statusEmail){   
                             case "false":
                                 return (
-                                    <a className="warning">correo electrónico no valido</a>
+                                    <a className="warning">¡correo electrónico no valido!</a>
                                 );
 
                                  
                                 }
+                            })()}
 
+                     <div>
+                         <label htmlFor="contraseña" className="text_login">Contraseña</label>
+                            <input type="password" className="input_login" name="contraseña" autoComplete="on" placeholder="Ingresa aquí tu contraseña" onChange={this.handleChange} />
+                     </div>    
+                     {(() => {
+                         switch (this.state.statusPassword) {
+                        case "false":
+                            return (
+                                <a className="warning">¡contraseña erronea!</a>
+                            );
+                         }
+                      })()}
+                            <br/>
+                           <button class="btn" onClick={this.login} >Aceptar</button>  
+                        
 
-                             })()}
+                            
+                             </div>
+                             </div>
                     
                               
-                     </div>
+                    
                                 
             
            
